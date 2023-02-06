17ef32cb1a9b130b161238511a4c4aa634d08eac
Bradley Mafra <53132092+BradleyMafra@users.noreply.github.com>
2023-02-06 17:49:40 -0300

    Spring

--- demo2/src/main/java/com/example/demo/Controller/clienteController.java
+++ demo2/src/main/java/com/example/demo/Controller/clienteController.java
@@ -7,23 +7,29 @@ import java.util.List;
 @RestController
 @RequestMapping("/cliente")
 public class clienteController {
+
     private clienteRepository clienteRepository = new clienteRepository();
+
     @GetMapping("/Buscar")
     public ResponseEntity< List<clienteModel> > getClientes() {
         return ResponseEntity.ok( clienteRepository.getClientes() );
     }
+
     @PostMapping("/Adicionar")
     public void addCliente(@RequestBody clienteModel cliente) {
         clienteRepository.addCliente(cliente);
     }
+
     @DeleteMapping("/Deletar")
     public void deleteCliente(@RequestBody clienteModel cliente) {
         clienteRepository.deleteCliente(cliente);
     }
+
     @PutMapping("/Atualizar")
     public void updateCliente(@RequestBody clienteModel cliente) {
         clienteRepository.updateCliente(cliente);
     }
+    
     @GetMapping("/Buscar/{id}")
     public ResponseEntity<clienteModel> getClienteById(@PathVariable int id) {
         return ResponseEntity.ok( clienteRepository.getClienteById(id) );

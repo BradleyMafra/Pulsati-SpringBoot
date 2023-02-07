f596bf86148c5fc263bc17115fb87660709711da
Bradley Mafra <53132092+BradleyMafra@users.noreply.github.com>
2023-02-07 18:06:47 -0300

    Atualizado

--- demo3/demo/src/main/java/com/example/demo/repository/usuariosRepository.java
+++ demo3/demo/src/main/java/com/example/demo/repository/usuariosRepository.java
@@ -1,7 +1,21 @@
 package com.example.demo.repository;
+
 import com.example.demo.model.usuariosModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
-public interface usuariosRepository extends JpaRepository<usuariosModel, Integer>{
+import java.util.List;
 
+public interface usuariosRepository extends JpaRepository<usuariosModel, Integer> {
+    public usuariosModel findByCPF(String CPF);
+
+    public List<usuariosModel> findByNOME(String NOME);
+
+    @Query(value = "SELECT * FROM BRADLEY_USUARIO WHERE upper(NOME) LIKE '%JAVA%'", nativeQuery = true)
+    public List<usuariosModel> encontraPessoaJava();
+
+    @Query(value = "SELECT * FROM BRADLEY_USUARIO WHERE NOME = ?1 AND   SEXO = ?2 AND   TIPO_SANGUINEO = ?3", nativeQuery = true)
+    public List<usuariosModel> pesquisaPorCaracteristicas(String NOME, String SEXO, String TIPO_SANGUINEO);
+    @Query(value = "SELECT * FROM bradley_usuario WHERE TRUNC((SYSDATE - DATA_NASCIMENTO) /365) > 18", nativeQuery = true)
+    public List<usuariosModel> maiorDeIdade();
 }

4de92ccaa6943a2f15e89a192145b1ee1f80efe2
Bradley Mafra <53132092+BradleyMafra@users.noreply.github.com>
2023-02-07 18:06:36 -0300

    Atualizado

--- APIFilme/APIFILME/src/main/java/com/example/APIFILME/repository/filmeRepository.java
+++ APIFilme/APIFILME/src/main/java/com/example/APIFILME/repository/filmeRepository.java
@@ -1,8 +1,17 @@
 package com.example.APIFILME.repository;
-
 import com.example.APIFILME.model.filmeModel;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.data.jpa.repository.Query;
+import java.util.List;
 public interface filmeRepository  extends JpaRepository<filmeModel, Integer> {
+
+    @Query(value = "SELECT * FROM EXERCICIO_FILME WHERE DATA_LANCAMENTO LIKE '%2023%'", nativeQuery = true)
+    public List<filmeModel> encontrarPelaData();
+    @Query(value = "SELECT * FROM EXERCICIO_FILME WHERE PAIS_ORIGEM = ?1", nativeQuery = true)
+    public List<filmeModel> encontrarPais(String PAIS_ORIGEM);
+    @Query(value = "SELECT * FROM EXERCICIO_FILME WHERE DATA_LANCAMENTO = ?1", nativeQuery = true)
+    public List<filmeModel> encontrarData(String DATA_LANCAMENTO);
+    @Query(value = "SELECT * FROM EXERCICIO_FILME ORDER BY quantidade_vendas DESC", nativeQuery = true)
+    public List<filmeModel> ordenarMaiorVenda();
 
 }

1c2d832e995fc3dc70f3d1016a71cfaeedc2e304
Bradley Mafra <53132092+BradleyMafra@users.noreply.github.com>
2023-02-08 16:34:25 -0300

    Api Tarefa

---
+++ APITarefas/src/main/java/com/example/APITarefas/repository/tarefaRepository.java
@@ -0,0 +1,13 @@
+package com.example.APITarefas.repository;
+import com.example.APITarefas.model.tarefaModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface tarefaRepository extends JpaRepository<tarefaModel, Integer> {
+
+    @Query(value = "SELECT * FROM TAREFAS WHERE UPPER(TRIM(STATUS)) = UPPER(?1)", nativeQuery = true)
+    public List<tarefaModel> encontrarStatus(String STATUS);
+    public List<tarefaModel> findByPRIORIDADE(int PRIORIDADE);
+}

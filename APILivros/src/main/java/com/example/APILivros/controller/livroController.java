47585727e4521fa6894dff94ca5a9195cfcd5a53
Bradley Mafra <53132092+BradleyMafra@users.noreply.github.com>
2023-02-09 10:14:07 -0300

    Atualizado

--- APILivros/src/main/java/com/example/APILivros/controller/livroController.java
+++ APILivros/src/main/java/com/example/APILivros/controller/livroController.java
@@ -3,10 +3,7 @@ import java.util.List;
 import com.example.APILivros.model.livroModel;
 import com.example.APILivros.repository.livroRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/livro")
@@ -15,6 +12,13 @@ public class livroController {
     private livroRepository repository;
     @GetMapping("/buscar")
     public List<livroModel> buscarTodos(){return repository.findAll();}
-    @GetMapping("/buscar/{autor}")
-    public livroModel buscarLivroPorAutor(@PathVariable String autor){return repository.findByAutor(autor);}
+    @GetMapping("/buscar/{id}")
+    public livroModel buscarPorId(@PathVariable int id){return repository.findById(id).get();}
+
+    @PostMapping("/adicionar")
+    public livroModel adicionarLivro(@RequestBody livroModel livro){return repository.save(livro);}
+    @PutMapping("/atualizar")
+    public livroModel atualizarLivro(@RequestBody livroModel livro){return repository.save(livro);}
+    @DeleteMapping("/deletar/{id}")
+    public void deletarLivro(@PathVariable int id){repository.deleteById(id);}
 }

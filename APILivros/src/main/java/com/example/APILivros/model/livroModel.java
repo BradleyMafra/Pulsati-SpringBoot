a156f4646ac60d4b82bc541a284b567acc29f069
Bradley Mafra <53132092+BradleyMafra@users.noreply.github.com>
2023-02-09 10:14:14 -0300

    Atualizado

--- APILivros/src/main/java/com/example/APILivros/model/livroModel.java
+++ APILivros/src/main/java/com/example/APILivros/model/livroModel.java
@@ -1,19 +1,23 @@
 package com.example.APILivros.model;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Data;
 
 @Data
-@Table(name = "livros")
+@Table(name = "LIVROS")
 @Entity
 public class livroModel{
-    @Column(name = "titulo")
+    @Id
+    @Column(name = "id")
+    int id;
+    @Column(name = "anoPublicacaoo")
+    int anoPublicacaoo;
+    @Column(name = "TITULO")
     String titulo;
     @Column(name = "autor")
     String autor;
-    @Column(name = "ano_publicacaoo")
-    int ano_publicacaoo;
     @Column(name = "genero")
     String genero;
 }

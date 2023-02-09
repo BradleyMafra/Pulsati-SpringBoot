40fe894fe80a8607e366ba74995d7fb62f9b3fe1
Bradley Mafra <53132092+BradleyMafra@users.noreply.github.com>
2023-02-09 16:19:42 -0300

    ApiExercicio

---
+++ APIExercicio/src/test/java/com/example/APIExercicio/ApiExercicioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.APIExercicio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiExercicioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

7753fe71aa3b873fd03fd94f8d1f22a391cc9fa2
Bradley Mafra <53132092+BradleyMafra@users.noreply.github.com>
2023-02-10 13:29:44 -0300

    ApiLoja

---
+++ APILoja/src/main/java/com/example/APILoja/model/vendaModel.java
@@ -0,0 +1,23 @@
+package com.example.APILoja.model;
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.sql.Date;
+
+@Data
+@Table(name = "VENDAS_BRADLEYEXEMPLO")
+@Entity
+public class vendaModel {
+    @Id
+    @Column(name = "id_venda")
+    int idVenda;
+    @ManyToOne
+    @JoinColumn(name = "id_produto",referencedColumnName = "id_produto")
+    private produtoModel produto;
+    @ManyToOne      // Aqui seria FK                       // AQUI O ID DA TABLE ABAIXO
+    @JoinColumn(name = "id_cliente",referencedColumnName = "id_cliente")
+    private clienteModel cliente;
+
+    @Column(name = "data_venda")
+    Date dataVenda;
+}

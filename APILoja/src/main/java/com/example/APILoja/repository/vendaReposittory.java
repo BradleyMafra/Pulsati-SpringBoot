80bb948f15801df8b3cadeb1639fee595962a30b
Bradley Mafra <53132092+BradleyMafra@users.noreply.github.com>
2023-02-10 13:29:50 -0300

    ApiLoja

---
+++ APILoja/src/main/java/com/example/APILoja/repository/vendaReposittory.java
@@ -0,0 +1,8 @@
+package com.example.APILoja.repository;
+
+import com.example.APILoja.model.vendaModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface vendaReposittory extends JpaRepository<vendaModel, Integer> {
+
+}

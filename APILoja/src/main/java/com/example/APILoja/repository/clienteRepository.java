f0b4adb7503e8f110e60beafe37903b0e80798c4
Bradley Mafra <53132092+BradleyMafra@users.noreply.github.com>
2023-02-10 13:29:36 -0300

    ApiLoja

---
+++ APILoja/src/main/java/com/example/APILoja/repository/clienteRepository.java
@@ -0,0 +1,7 @@
+package com.example.APILoja.repository;
+import com.example.APILoja.model.clienteModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface clienteRepository extends JpaRepository<clienteModel, Integer> {
+
+}
